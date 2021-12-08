da739a42b46259a178c4e6943899cbc30623aebc
Ayrton <devayrtonsbarreto@gmail.com>
2021-12-08 10:40:30 -0300

    Criando um cargo e salvando no banco de dados

--- src/main/java/br/com/ayrton/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/ayrton/spring/data/SpringDataApplication.java
@@ -1,13 +1,30 @@
 package br.com.ayrton.spring.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.ayrton.spring.data.orm.Cargo;
+import br.com.ayrton.spring.data.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+	private final CargoRepository cargoRepository;
+
+	public SpringDataApplication(CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo("DESENVOLVEDOR DE SOFTWARE");
+		cargoRepository.save(cargo);
 	}
 
 }
--- src/main/java/br/com/ayrton/spring/data/orm/Cargo.java
+++ src/main/java/br/com/ayrton/spring/data/orm/Cargo.java
@@ -9,23 +9,30 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "cargos")
 public class Cargo {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
+
+	public Cargo(String descricao) {
+		this.descricao = descricao;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	
+
 }
